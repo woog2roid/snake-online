71d4abe063daa8ebe3f3fc1a8d7b616609b2334c
woog2roid <wooguijung@korea.ac.kr>
2021-04-03 21:05:42 +0900

    회원가입 로그인 프론트엔드 구성 완료

--- routes/page.js
+++ routes/page.js
@@ -4,5 +4,12 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('index.html');
 });
+router.get('/join', (req, res) => {
+    res.render('join.html');
+});
+router.get('/login', (req, res) => {
+    res.render('login.html');
+});
+
 
 module.exports = router;
